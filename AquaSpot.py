f8d9ac344e6467a82fa4670abf89721cb2d93d1e
SHANJAY975 <shanjay21cs082@jit.ac.in>
2023-03-06 06:41:22 +0530

    Modified few lines of code in AquaSpot and Added 3 new buttons for vessel typed

--- AquaSpot.py
+++ AquaSpot.py
@@ -1,10 +1,9 @@
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QWidget, QFileDialog
-import pandas as pd
+from pandas import pandas as pd
 import folium
 from datetime import datetime
-df = pd.read_csv('Dataset/Dataset.csv')
-df['date_time']=0  
+
 class MapWindow(QMainWindow):
     def __init__(self,x,df):
         super().__init__()
@@ -110,10 +109,34 @@ class SampleWindow(QMainWindow):
     
         # Create the horizontal layout and add widgets to it
         horizontal_layout = QHBoxLayout()
+        horizontal_layout1 = QHBoxLayout()
+        # Add widgets to the horizontal layout
+        label = QLabel("Select a map plot")
         
+        label0 = QLabel("Vessel Type")
+        button5 = QPushButton("Drifting_longlines")
+        button6 = QPushButton("Fixed_gear")
+        button7 = QPushButton("Pole_and_line")
+        button8 = QPushButton("trollers")
+        button9 = QPushButton("Purse_seines")
+
+        button5.clicked.connect(lambda: self.drifting_longlines())
+        button6.clicked.connect(lambda: self.fixed_gear())
+        button7.clicked.connect(lambda: self.pole_and_line())
+        button8.clicked.connect(lambda: self.trollers())
+        button9.clicked.connect(lambda: self.purse_seiners())
+
+        horizontal_layout.addWidget(label)
+        horizontal_layout.addWidget(label0)
+
+        horizontal_layout1.addWidget(button5)
+        horizontal_layout1.addWidget(button6)
+        horizontal_layout1.addWidget(button7)
+        horizontal_layout1.addWidget(button8)
+        horizontal_layout1.addWidget(button9)
         # Add the horizontal layout to the main layout
         layout.addLayout(horizontal_layout)
-
+        layout.addLayout(horizontal_layout1)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
@@ -142,7 +165,12 @@ class SampleWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication([])
+    df = pd.read_csv('Dataset/Dataset.csv')
+    df['date_time']=0  
     sample_window = SampleWindow(0,df)
     sample_window.show()
     app.exec()
+
+
+
 
